294b64f8c8ca9b31203715c4c2046702ce451219
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-01-10 18:45:01 +0530

    IB Action of Height and Weight are connected

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  BMI Calculator
 //
-//  Created by Angela Yu on 21/08/2019.
+//  Created by Vaibhav Bisht on 10/01/2020.
 //  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
@@ -14,7 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func HeightSlider(_ sender: UISlider) {
+        let Height = String(format: "%2f", sender.value)
+        print(Height)
+    }
+    
+    @IBAction func WeightSlider(_ sender: UISlider) {
+        let Weight = String(format: "%2f", sender.value)
+        print(Weight)
+    }
 }
 
