48f07d865ddc00c6db4b1ae22ae4645e5d90955f
Vaibhav Bisht <vaibhavbisht06@gmail.com>
2020-01-11 21:02:08 +0530

    Initial Commit @Time

---
+++ BMI Calculator/Controllers/SecondVC.swift
@@ -0,0 +1,14 @@
+//
+//  SecondVC.swift
+//  BMI Calculator
+//
+//  Created by Vaibhav Bisht on 11/01/20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class  SecondVC: UIViewController {
+    <#code#>
+}
--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -3,7 +3,6 @@
 //  BMI Calculator
 //
 //  Created by Vaibhav Bisht on 10/01/2020.
-//  Copyright © 2019 Angela Yu. All rights reserved.
 //
 
 import UIKit
@@ -16,20 +15,29 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var weightSliderOutlet: UISlider!
+    @IBOutlet weak var heightSliderOutlet: UISlider!
+    
     
     @IBAction func heightSlider(_ sender: UISlider) {
         let height = String(format: "%.2f", sender.value)
         heightLabel.text = "\(height)m"
         print(height)
     }
+    
     @IBAction func weightSlider(_ sender: UISlider) {
         let weight = String(format: "%.0f", sender.value)
         weightLabel.text = "\(weight)Kg"
         print(weight)
     }
-    @IBAction func calculateButtonPressed(_ sender: UIButton) {
-    }
     
-
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        let Height = heightSliderOutlet.value
+        print(heightSliderOutlet.value)
+        let Weight = weightSliderOutlet.value
+        print(weightSliderOutlet.value)
+        let BMI = Weight/(Height*Height)
+        print(BMI)
+    } 
 }
 
